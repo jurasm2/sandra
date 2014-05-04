49dc6606df49463827daf32d21bf8ce376c5d0fc
Marek Juras <marek.juras@lundegaard.eu>
2014-05-04 15:35:56 +0200

    form facelift + logo

--- app/components/forms/BaseForm.php
+++ app/components/forms/BaseForm.php
@@ -12,7 +12,27 @@ class BaseForm extends Nette\Application\UI\Form
     {
         parent::__construct($parent, $name);
 
+        $this->getElementPrototype()->class = 'form-inline';
+
         // ajaxify the form
         // $this->getElementPrototype()->class = 'ajax';
     }
+
+    public function addText($name, $label = NULL, $cols = NULL, $maxLength = NULL) {
+        $text = parent::addText($name, $label, $cols, $maxLength);
+
+        $controlPrototype = $text->getControlPrototype();
+        $controlPrototype->class = "form-control";
+        $controlPrototype->placeholder = $label;
+
+        $text->getLabelPrototype()->class = "sr-only";
+        return $text;
+    }
+
+    public function addSubmit($name, $caption = NULL) {
+        $submit = parent::addSubmit($name, $caption);
+        $submit->getControlPrototype()->class = 'btn btn-default';
+        return $submit;
+    }
+
 }
